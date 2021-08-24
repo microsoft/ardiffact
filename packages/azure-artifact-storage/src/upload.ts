f181144a4639edbd694e2a2032bc982a22a9e39e
Larry Kiniu <lkiniu@microsoft.com>
2021-08-24 15:42:10 +0300

    add documentation for the uploadArtifacts API

--- packages/azure-artifact-storage/src/upload.ts
+++ packages/azure-artifact-storage/src/upload.ts
@@ -3,6 +3,15 @@ import { AzureBlobStorageConfig } from "./config";
 import * as fs from "fs";
 import * as path from "path";
 
+/**
+ * 
+ * @param dirPath - directory path containing the artifacts to upload
+ * @param prefix - prefix to append to the file name which makes the blob name to be uploaded to Azure Blob Storage
+ * @param config - the {@link @ardiffact/azure-artifact-storage#AzureBlobStorageConfig} object used to create an instance of a Storage client
+ * @param gzip - whether to compress the artifacts using Gzip
+ * @returns empty Promise
+ * @public
+ */
 export const uploadArtifacts = async (
   dirPath: string,
   prefix: string,
