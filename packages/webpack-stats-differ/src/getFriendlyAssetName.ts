09a529e04351de0df18b3d5be8e9bd0686756a91
derricknjeru <derricknjeru@microsoft.com>
2023-05-04 12:27:30 +0300

    simplified removehash function

--- packages/webpack-stats-differ/src/getFriendlyAssetName.ts
+++ packages/webpack-stats-differ/src/getFriendlyAssetName.ts
@@ -22,9 +22,9 @@ export function getFriendlyAssetName(
 ): string {
   // First try removing the hash manually from the file name.
   const name = asset.name;
-  const nameWithoutHash = removeHashFromName(name);
-  if (nameWithoutHash !== name) {
-    return nameWithoutHash;
+  const modifiedFileName = cleanFileName(name);
+  if (modifiedFileName !== name) {
+    return modifiedFileName;
   }
 
   // Try using the chunk names from the asset data instead.
@@ -37,48 +37,12 @@ export function getFriendlyAssetName(
   return name;
 }
 
-const hashRegex = /_([a-z0-9]{20})/;
+const removeHashFromName = (name: string): string => {
+  return name.replace(/([_.][a-z0-9]{20})(?:\b)/, "");
+};
 
-// TODO(mapol): We need a better way to identify a hash inside a filename
-// TODO(mapol): We need to make this function take into account the folder name. Once we do that we can remove the `ignoreDelveStringsFiles` function.
-export const removeHashFromName = (name: string): string => {
+const cleanFileName = (name: string): string => {
   const fileParts = upath.parse(name);
-  // We could have filenames like .d.ts or .js.map
-  const splitByDotPart = fileParts.name.split(".");
-  const firstNamePart = splitByDotPart[0];
-  const secondDotPart = splitByDotPart[1];
-  const thirdDotPart = splitByDotPart[2];
-  // Remove hash
-  const otherParts = firstNamePart.split("_");
-
-  if (
-    otherParts.length === 1 ||
-    otherParts[otherParts.length - 1].length !== 20
-  ) {
-    // Search uses a hash in the secondDotPart e.g. search_init.min_7ba0dc2ea2246e82e8a5.js
-    // Oneshell uses a hash in the fourth part e.g. suiteux.shell.bootstrapper.d5a8a72572eca5ebbdd9.js
-
-    const cleanSecondPart =
-    secondDotPart && // Check if secondDotPart is truthy
-    hashRegex.test(secondDotPart) // If it's truthy, check if it matches the hashRegex
-      ? secondDotPart.replace(hashRegex, "") // If it matches, set the matched pattern removed from it using the replace() method
-      : thirdDotPart // If it doesn't match, check if thirdDotPart is truthy
-      ? secondDotPart + "." + thirdDotPart.replace(hashRegex, "") // If it's truthy, concatenate secondDotPart with a period and thirdDotPart with any matched pattern in thirdDotPart replaced with an empty string
-      : secondDotPart; // If neither condition is met, set cleanSecondPart to secondDotPart
-
-    return (
-      [
-        otherParts.join("_"),
-        cleanSecondPart, 
-      ]
-        .filter(identity)
-        .join(".") + fileParts.ext
-    );
-  }
-
-  return (
-    [otherParts.slice(0, -1).join("_"), secondDotPart]
-      .filter(identity)
-      .join(".") + fileParts.ext
-  );
+  const baseName = fileParts.name;
+  return removeHashFromName(baseName) + fileParts.ext;
 };
