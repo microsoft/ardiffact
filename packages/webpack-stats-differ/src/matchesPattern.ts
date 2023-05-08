e3337b3d841563df8fe21e8ef6377f86aefb0cbf
derricknjeru <derricknjeru@microsoft.com>
2023-05-08 10:29:47 +0300

    clean up the code

--- packages/webpack-stats-differ/src/matchesPattern.ts
+++ packages/webpack-stats-differ/src/matchesPattern.ts
@@ -4,3 +4,5 @@ export const matchesPattern = (
   filter: string | string[] | undefined,
   input: string
 ) => (filter ? isMatch(input, filter) : true);
+
+export const hashRegex = /^.*[\/]|([_.][a-z0-9]{20})(?:\b)/g;
